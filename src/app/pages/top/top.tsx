6888d5460f9741934d40e4d4ed8e787c5ab176b5
yoji5635 <satsumaimo697@gmail.com>
2024-10-13 21:18:03 +0900

    背景色の定義

--- src/app/pages/top/top.tsx
+++ src/app/pages/top/top.tsx
@@ -1,15 +1,16 @@
-'use client'
+'use client';
 
-import { Header } from "@/app/pages/top/components/header/header"
-import '@i18n'
+import { Header } from '@/app/pages/top/components/header/header';
+import styles from './top.module.scss';
+import '@i18n';
+import { Footer } from '@/app/pages/top/components/header/footer';
 
 export const Top: React.FC = () => {
   return (
-    <>
-    <Header></Header>
-    <div>
-      Top
+    <div className={styles.container}>
+      <Header></Header>
+      <div className={styles.main}>Top</div>
+      <Footer></Footer>
     </div>
-    </>
-  )
-}
+  );
+};
