b94413739fa0c4e8439e12555267ef97d0aee7d5
yoji5635 <satsumaimo697@gmail.com>
2024-10-13 22:55:31 +0900

    改行調整

--- src/app/pages/top/components/header/header-menu/header-menu.tsx
+++ src/app/pages/top/components/header/header-menu/header-menu.tsx
@@ -1,6 +1,6 @@
+'use client';
 
-'use client'
-import styles from './header-menu.module.scss'
+import styles from './header-menu.module.scss';
 import { useTranslation } from 'react-i18next';
 import { FC } from 'react';
 // import '@i18n/i18n'
@@ -8,11 +8,11 @@ import { FC } from 'react';
 export const HeaderMenu: FC = () => {
   const { t } = useTranslation('common');
   return (
-    <div className={ styles.menu }>
-      <div>{ t('top.header.aboutMe') }</div>
-      <div>{ t('top.header.service') }</div>
-      <div>{ t('top.header.recruit') }</div>
-      <div>{ t('top.header.contact') }</div>
+    <div className={styles.menu}>
+      <div>{t('top.header.aboutMe')}</div>
+      <div>{t('top.header.service')}</div>
+      <div>{t('top.header.recruit')}</div>
+      <div>{t('top.header.contact')}</div>
     </div>
-  )
-}
+  );
+};
