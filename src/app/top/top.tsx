d5a704637bccfb5b089a1279533dad407ad3fbc5
yoji5635 <satsumaimo697@gmail.com>
2024-10-13 11:03:55 +0900

    topページの作成

---
+++ src/app/top/top.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/app/top/components/header/header"
+
+export const Top: React.FC = () => {
+  return (
+    <>
+    <Header></Header>
+    <div>
+      Top
+    </div>
+    </>
+  )
+}
