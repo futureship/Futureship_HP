3befd1d7e805b5f912b226369becee658af6daf8
yoji5635 <satsumaimo697@gmail.com>
2024-10-14 17:32:26 +0900

    ページの作成

---
+++ src/app/about/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import styles from './about.module.scss';
+import '@i18n';
+import { useTranslation } from 'react-i18next';
+
+const About: React.FC = () => {
+  const { t } = useTranslation('common');
+  const history = t('about.contents.history')
+    .split('\n')
+    .map((line) => (
+      <tr>
+        <td className={styles.historyTd}>{line.split(':')[0]}</td>
+        <td>{line.split(':')[1]}</td>
+      </tr>
+    ));
+
+  return (
+    <div className={styles.container}>
+      <div>{t('about.title')}</div>
+
+      <table>
+        <tbody>
+          <tr>
+            <td className={styles.companyProfileTd}>
+              {t('about.companyName')}
+            </td>
+            <td>{t('about.contents.companyName')}</td>
+          </tr>
+          <tr>
+            <td className={styles.companyProfileTd}>
+              {t('about.foundationDay')}
+            </td>
+            <td>{t('about.contents.foundationDay')}</td>
+          </tr>
+          <tr>
+            <td className={styles.companyProfileTd}>{t('about.address')}</td>
+            <td>{t('about.contents.address')}</td>
+          </tr>
+          <tr>
+            <td className={styles.companyProfileTd}>{t('about.ceo')}</td>
+            <td>{t('about.contents.ceo')}</td>
+          </tr>
+          <tr>
+            <td className={styles.companyProfileTd}>{t('about.business')}</td>
+            <td>{t('about.contents.business')}</td>
+          </tr>
+          <tr>
+            <td className={styles.companyProfileTd}>{t('about.vision')}</td>
+            <td>{t('about.contents.vision')}</td>
+          </tr>
+          <tr>
+            <td className={styles.companyProfileTd}>{t('about.mission')}</td>
+            <td className={styles.preLine}>{t('about.contents.mission')}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table>
+        <thead>
+          <tr>
+            <th>{t('about.history')}</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{history}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default About;
