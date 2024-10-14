0b5b8c5da26d49118edf338d0959849033a4ebf7
yoji5635 <satsumaimo697@gmail.com>
2024-10-14 18:07:17 +0900

    ポインタースタイル

--- src/app/pages/top/components/header/header-menu/header-menu.tsx
+++ src/app/pages/top/components/header/header-menu/header-menu.tsx
@@ -3,16 +3,15 @@
 import styles from './header-menu.module.scss';
 import { useTranslation } from 'react-i18next';
 import { FC } from 'react';
-// import '@i18n/i18n'
 
 export const HeaderMenu: FC = () => {
   const { t } = useTranslation('common');
   return (
     <div className={styles.menu}>
-      <div>{t('top.header.aboutMe')}</div>
-      <div>{t('top.header.service')}</div>
-      <div>{t('top.header.recruit')}</div>
-      <div>{t('top.header.contact')}</div>
+      <div className={styles.cursor}>{t('top.header.aboutMe')}</div>
+      <div className={styles.cursor}>{t('top.header.service')}</div>
+      <div className={styles.cursor}>{t('top.header.recruit')}</div>
+      <div className={styles.cursor}>{t('top.header.contact')}</div>
     </div>
   );
 };
