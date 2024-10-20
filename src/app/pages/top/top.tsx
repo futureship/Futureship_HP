9dd48d5534fa11993bf8a8687cbf968beb5ef850
yoji5635 <satsumaimo697@gmail.com>
2024-10-20 10:41:53 +0900

    文字列スタイルの適用

--- src/app/pages/top/top.tsx
+++ src/app/pages/top/top.tsx
@@ -13,9 +13,9 @@ export const Top: React.FC = () => {
     <div className={styles.container}>
       <Header></Header>
       <div className={styles.main}>
-        <div>{t('top.title.subTitle')}</div>
-        <div>{t('top.title.title1')}</div>
-        <div>{t('top.title.title2')}</div>
+        <div className={styles.subTitle}>{t('top.title.subTitle')}</div>
+        <div className={styles.title}>{t('top.title.title1')}</div>
+        <div className={styles.title}>{t('top.title.title2')}</div>
       </div>
       <Footer></Footer>
     </div>
