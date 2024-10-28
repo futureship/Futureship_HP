80cc47e5c4c72a5b4954b5697f6658f166f42259
yoji5635 <satsumaimo697@gmail.com>
2024-10-28 11:59:30 +0900

    サービス文字入れ

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -6,26 +6,46 @@ import { useTranslation } from 'react-i18next';
 
 const Service: React.FC = () => {
   const { t } = useTranslation('common');
+  const service1Description1 = t('service.service1.description1')
+    .split('\n')
+    .map((line) => <div>{line}</div>);
+
+  const service1Description2 = t('service.service1.description2')
+    .split('\n')
+    .map((line) => <div>{line}</div>);
+
+  const service2Description1 = t('service.service2.description1')
+    .split('\n')
+    .map((line) => <div>{line}</div>);
+
+  const service2Description2 = t('service.service2.description2')
+    .split('\n')
+    .map((line) => <div>{line}</div>);
 
   return (
     <div>
-      <img src=""></img>
-      <div>service</div>
+      <div className={styles.title}>{t('service.title')}</div>
       <div className={styles.service1}>
-        <img src=""></img>
+        <img src="/assets/svg/service1-img.svg"></img>
         <div>
-          <div>つむぐ</div>
-          <div>{}</div>
-          <button></button>
+          <div>{t('service.service1.title')}</div>
+          <div>{service1Description1}</div>
+          <div>{service1Description2}</div>
+          <button className={styles.button}>
+            {t('service.service1.serviceAbout')}
+          </button>
         </div>
       </div>
       <div className={styles.service2}>
         <div>
-          <div>ミラプロ</div>
-          <div>{}</div>
-          <button className={styles.button}>ミラプロを知る</button>
+          <div>{t('service.service2.title')}</div>
+          <div>{service2Description1}</div>
+          <div>{service2Description2}</div>
+          <button className={styles.button}>
+            {t('service.service2.serviceAbout')}
+          </button>
         </div>
-        <img src=""></img>
+        <img src="/assets/svg/service2-img.svg"></img>
       </div>
 
       <div>{}</div>
