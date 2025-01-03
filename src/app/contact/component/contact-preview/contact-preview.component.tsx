0b327bd88297de4b6373229deaf29155eeb3350c
yoji5635 <satsumaimo697@gmail.com>
2025-01-03 17:13:02 +0900

    プレビュー追加

--- src/app/contact/component/contact-preview/contact-preview.component.tsx
+++ src/app/contact/component/contact-preview/contact-preview.component.tsx
@@ -11,8 +11,17 @@ import {
 
 export const ContactPreview: React.FC = () => {
   const { t } = useTranslation('common');
-  const { updateIsEdit, name, email, phone, place, position, kind, contents } =
-    useContactViewModel();
+  const {
+    updateIsEdit,
+    name,
+    email,
+    phone,
+    place,
+    position,
+    kind,
+    contents,
+    files,
+  } = useContactViewModel();
 
   const kindPreviewList = [];
   contactKindEnumKeys().forEach((key) => {
@@ -21,6 +30,11 @@ export const ContactPreview: React.FC = () => {
     }
   });
   // const kindPreview = kindPreviewList.
+
+  const addedFiles = files.map((file) => (
+    <div className={styles.files}>{file.name}</div>
+  ));
+
   return (
     <div className={styles.previewArea}>
       <div className={styles.title}>{t('contact.check.title')}</div>
@@ -65,7 +79,10 @@ export const ContactPreview: React.FC = () => {
         <span className={styles.columnName}>
           {t('contact.check.column.contents')}
         </span>
-        <span className={styles.columnContents}>{contents}</span>
+        <span className={styles.columnContents}>
+          {contents}
+          {addedFiles}
+        </span>
       </div>
       <button className={styles.edit} onClick={() => updateIsEdit(true)}>
         {t('contact.check.button.edit')}
